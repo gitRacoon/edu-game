f0abd4d1f0663ca04ef2c7b2713f7f4b59f27662
Kolia Zamnius <kolia.zamnius@faifly.com>
2021-07-27 12:14:00 +0300

    bord nav via component

---
+++ src/components/BordNav.vue
@@ -0,0 +1,117 @@
+<template>
+  <p class="lvl">
+    Complexity: <strong>{{ lvl }}</strong>
+  </p>
+  <div class="buttons">
+    <router-link to="/" class="menu-link" aria-label="Go to the menu." />
+    <button
+      class="play-btn"
+      :class="[behavior]"
+      :disabled="shutdown"
+      @click="$emit('report', behavior)"
+    >
+      {{ behavior }}
+    </button>
+    <button class="cup-btn" aria-label="Open a list of achievements."></button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["report"],
+  props: {
+    behavior: {
+      type: String,
+      required: true,
+    },
+    shutdown: {
+      type: Boolean,
+      required: true,
+    },
+    lvl: {
+      type: [Number, String],
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.lvl {
+  font: 600 1.2rem/1.2rem "Zen Loop", cursive;
+  letter-spacing: 0.1rem;
+}
+
+.buttons {
+  display: flex;
+  place-content: center;
+  place-items: center;
+  gap: 15px;
+
+  margin-top: 15px;
+}
+
+.menu-link {
+  width: 30px;
+  height: 20px;
+
+  background: url("../assets/menu.svg") no-repeat center/contain;
+
+  &:hover {
+    opacity: 0.85;
+  }
+}
+
+.play-btn {
+  display: flex;
+  place-items: center;
+  place-content: center;
+
+  width: 150px;
+  height: 40px;
+
+  font: bold 1.5rem/1.5rem "Zen Loop", cursive;
+  color: white;
+  text-transform: capitalize;
+
+  border: none;
+  border-radius: 5px;
+  background-color: lightseagreen;
+
+  user-select: none;
+  cursor: pointer;
+  opacity: 1;
+  transition: all 0.2s linear;
+
+  &.restart {
+    background-color: darksalmon;
+  }
+
+  &.lose {
+    background-color: tomato;
+  }
+
+  &:hover {
+    opacity: 0.85;
+  }
+
+  &:disabled {
+    background-color: lightgray;
+  }
+}
+
+.cup-btn {
+  width: 30px;
+  height: 30px;
+
+  border: none;
+  background: url("../assets/cup.svg") no-repeat center/contain;
+
+  cursor: pointer;
+  transition: all 0.2s linear;
+
+  &:hover {
+    opacity: 0.85;
+  }
+}
+</style>
--- src/components/Classic.vue
+++ src/components/Classic.vue
@@ -16,29 +16,20 @@
         @click="checkup(foo)"
       />
     </div>
-    <p class="lvl">
-      Complexity: <strong>{{ lvl }}</strong>
-    </p>
-    <div class="buttons">
-      <router-link to="/" class="menu-link" aria-label="Go to the menu." />
-      <button
-        class="play-btn"
-        :class="[behavior]"
-        :disabled="shutdown"
-        @click="report()"
-      >
-        {{ behavior }}
-      </button>
-      <button
-        class="cup-btn"
-        aria-label="Open a list of achievements."
-      ></button>
-    </div>
+    <bord-nav
+      :behavior="behavior"
+      :lvl="lvl"
+      :shutdown="shutdown"
+      @report="init"
+    />
   </main>
 </template>
 
 <script>
+import BordNav from "./BordNav.vue";
+
 export default {
+  components: { BordNav },
   data() {
     return {
       behavior: "start",
@@ -61,87 +52,56 @@ export default {
     return (this.visibleField = [...this.initField]);
   },
   methods: {
-    report() {
-      //* Реагирует на главную кнопку.
-      switch (this.behavior) {
+    init(value) {
+      switch (value) {
         case "start":
+          this.lvl = 0;
         case "next":
-          this.advance();
-          break;
-        case "replay":
-          this.setzero();
-          this.advance();
-          break;
-        case "restart":
-          this.locker();
-          break;
-        default:
+          this.initField[this.lvl].order = true;
+          this.lvl++;
           break;
       }
-      this.found = 0;
-      this.dismiss();
 
-      this.reveal();
-      setTimeout(this.reveal, 1000);
-      setTimeout(this.locker, 1000);
+      this.lock = true;
+      this.shutdown = true;
+      this.found = 0;
+      this.behavior = "restart";
+
+      for (let foo = 0; foo < 25; foo++) {
+        this.visibleField[foo].pick = false;
+      }
+      for (let foo = 0; foo < this.lvl; foo++) {
+        this.initField[foo].show = true;
+      }
 
       this.visibleField.sort(() => Math.random() - 0.5);
+
+      setTimeout(() => {
+        for (let foo = 0; foo < this.lvl; foo++) {
+          this.initField[foo].show = false;
+        }
+        this.shutdown = false;
+        this.lock = false;
+      }, 1000);
     },
     checkup(foo) {
-      //* Реагирует на выбор ячейки.
       this.visibleField[foo].pick = true;
 
       if (this.visibleField[foo].order) {
         this.found++;
       } else {
         this.behavior = "lose";
-        this.locker();
+        this.lock = true;
       }
 
       if (this.found === this.lvl && this.lvl < 12) {
         this.behavior = "next";
-        this.locker();
+        this.lock = true;
       } else if (this.found === this.lvl && this.lvl === 12) {
         this.lvl = "MAX";
-        this.behavior = "replay";
-        this.locker();
+        this.behavior = "start";
+        this.lock = true;
       }
-    },
-    reveal() {
-      //* Переключает видимость загаданных ячеек.
-      this.shutdown = !this.shutdown;
-      for (let foo = 0; foo < this.lvl; foo++) {
-        this.initField[foo].show = !this.initField[foo].show;
-      }
-    },
-    advance() {
-      //* Увеличивает сложность.
-      if (this.lvl < 12) {
-        this.initField[this.lvl].order = true;
-        this.lvl++;
-      }
-    },
-    dismiss() {
-      //* Очищает выбранные ячейки.
-      for (let foo = 0; foo < 25; foo++) {
-        this.visibleField[foo].pick = false;
-      }
-
-      this.behavior = "restart";
-      this.shutdown = false;
-    },
-    setzero() {
-      //* Обнуляет поле.
-      this.lvl = 0;
-      for (let foo = 0; foo < 25; foo++) {
-        this.visibleField[foo].order = false;
-        this.visibleField[foo].pick = false;
-        this.visibleField[foo].show = false;
-      }
-    },
-    locker() {
-      //* Блокирует и разблокирует поле.
-      this.lock = !this.lock;
     },
   },
 };
@@ -189,10 +149,5 @@ export default {
     border: 2px solid tomato;
     box-shadow: inset 0 0 10px tomato;
   }
-}
-
-.lvl {
-  font: 600 1.2rem/1.2rem "Zen Loop", cursive;
-  letter-spacing: 0.1rem;
 }
 </style>
